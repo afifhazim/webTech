a98019d048026b9bf9ab0cabc9b4c97ad87b3b12
afifhazim <49102339+afifhazim@users.noreply.github.com>
2019-11-10 15:51:28 +0800

    Add files via upload

---
+++ classActivity/jsFile/week7class2.js
@@ -0,0 +1,20 @@
+//count.js
+var number1 = 10;
+var number2 = 20;
+var myNumber1 = '3';
+var myNumber2 = '4';
+var count1 = number1 + number2;
+var count2 = myNumber1 + myNumber2;
+var el = document.getElementById('one');
+el.textContent = count1;
+var el = document.getElementById('two');
+el.textContent = count2;
+
+/*var num = 47;
+var address = ‘Jalan Gombak’;
+var add = num + address;
+
+var num3 = 50;
+var num4 = 100;
+var multiply = num3 * num4;
+*/
